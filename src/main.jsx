32b9af1e9d0295c611c6a5cbc9bdc2ff77bfb654
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-10-18 10:09:56 +0200

    ready to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,16 @@
+if (process.env.NODE_ENV === "development") {
+	import("react-dom").then(({ __REACT_DEVTOOLS_GLOBAL_HOOK__ }) => {
+		if (typeof __REACT_DEVTOOLS_GLOBAL_HOOK__ === "object") {
+			for (const [key, value] of Object.entries(
+				__REACT_DEVTOOLS_GLOBAL_HOOK__
+			)) {
+				__REACT_DEVTOOLS_GLOBAL_HOOK__[key] =
+					typeof value === "function" ? () => {} : null;
+			}
+		}
+	});
+}
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
