19a575ce93fd433db6871539c19dd321d022bf1f
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-10-18 10:12:30 +0200

    rettet fejl i referance til gallrey

--- src/Components/Customers/Home.jsx
+++ src/Components/Customers/Home.jsx
@@ -1,5 +1,5 @@
 import { Footer } from "../Footer/Footer";
-import { Gallery } from "../gallery/Gallery";
+import { Gallery } from "../Gallery/Gallery.jsx";
 
 import { Links } from "../Links/Links";
 import { Logo } from "../Logo/Logo";
